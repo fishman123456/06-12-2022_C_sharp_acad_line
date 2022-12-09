ef188b1fb12490d03685eacc7c6e3f89436f2e6b
denis <106389581+fishman123456@users.noreply.github.com>
2022-12-09 12:45:16 +0700

    добавил кнопку выход

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,13 @@
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,27 +46,15 @@
             this.button1.BackColor = System.Drawing.SystemColors.Window;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 27);
+            this.button1.Size = new System.Drawing.Size(163, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "Считать файл";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(114, 3);
-            this.textBox1.MinimumSize = new System.Drawing.Size(10, 10);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(1025, 470);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // tableLayoutPanel1
             // 
@@ -72,55 +63,106 @@
             this.tableLayoutPanel1.AutoScrollMinSize = new System.Drawing.Size(100, 100);
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1142, 536);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(949, 397);
             this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(172, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(774, 294);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.SystemColors.Window;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(3, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 27);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Выход";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button2
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button2.AutoSize = true;
             this.button2.BackColor = System.Drawing.SystemColors.Window;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(5, 492);
+            this.button2.Location = new System.Drawing.Point(3, 303);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 27);
+            this.button2.Size = new System.Drawing.Size(163, 24);
             this.button2.TabIndex = 4;
             this.button2.Text = "Делим файл";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.SystemColors.Window;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(3, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 24);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Записываем структуру";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // textBox2
             // 
             this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(114, 479);
-            this.textBox2.Multiline = true;
+            this.textBox2.Location = new System.Drawing.Point(172, 303);
             this.textBox2.Name = "textBox2";
-            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(1025, 54);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Size = new System.Drawing.Size(774, 23);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Location = new System.Drawing.Point(172, 333);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(774, 23);
+            this.textBox3.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1142, 536);
+            this.ClientSize = new System.Drawing.Size(949, 397);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "U83";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -132,9 +174,12 @@
 
         private OpenFileDialog openFileDialog1;
         private Button button1;
-        private TextBox textBox1;
         private TableLayoutPanel tableLayoutPanel1;
-        private TextBox textBox2;
         private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,13 +11,25 @@ namespace _06_12_2022_C_sharp_acad_line
 
     public partial class Form1 : Form
     {
-        public string filename_f =null;
+
+        public struct CadCoord // создаем структуру для храниния извлеченных данных из автокада 8 полей
+        {
+            public string number_p; // номер по порядку 1)
+            public string name_block; // имя блока в автокаде "ElectrikKabinet" - пример 2)
+            public string cordX; // Координата X  20422.6312	- пример 3)
+            public string cordY; // Координата Y  29950.5828    - пример 4)
+            public string cordZ;  // Координата Z 	0.0000      - пример 5)
+            public string name_kable; // имя кабеля 101.2-K1       - пример 6)
+            public string beginning; // начало (откуда идет кабель)  Щит ШУК101   - пример 7)
+            public string ending; // // конец (куда идет кабель)    	Щит ШСК101  - пример 8)
+        }
+        public string filename_f = null;
         public int count_string = 0;
 
         public Form1()
         {
             InitializeComponent();
-            
+
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -26,16 +38,16 @@ namespace _06_12_2022_C_sharp_acad_line
             //    string something = File.ReadAllText(filename);
             //}
             //открываем диалог для выбора файла
-                       OpenFileDialog OPF = new OpenFileDialog();
+            OpenFileDialog OPF = new OpenFileDialog();
             // путь по умолчанию
             OPF.InitialDirectory = @"C:\Users\Fishman.DB.CORP\Documents\GitHub
             \06-12-2022_C_sharp_acad_line\bin\Debug\net6.0-windows";
             if (OPF.ShowDialog() == DialogResult.OK)
             {
-               // MessageBox.Show(OPF.FileName, "Файл считан");
-                filename_f= OPF.FileName;
-               // Stream stream = OPF.OpenFile();
-         // выводим файл в TextBox
+                // MessageBox.Show(OPF.FileName, "Файл считан");
+                filename_f = OPF.FileName;
+                // Stream stream = OPF.OpenFile();
+                // выводим файл в TextBox
                 textBox1.Text = File.ReadAllText(filename_f);
                 // разбиваем файл на подстроки, где в дальнейшем
                 // будем искать ключи и значения для словарей
@@ -44,28 +56,79 @@ namespace _06_12_2022_C_sharp_acad_line
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) // кнопка для создания массива строк
         {
             // для заволнения списка строками
             //List<string> myList = new List<string>();
-               if (filename_f!=null) {  
-            
-             //   textBox2.Text = File.ReadAllText(filename_f);  // TODO выводим содержимое файла в textbox2
+            if (filename_f != null)
+            {
+
+                //   textBox2.Text = File.ReadAllText(filename_f);  // TODO выводим содержимое файла в textbox2
                 string[] MassString = File.ReadAllLines(filename_f); // TODO создаем массив строк
-                textBox2.Text = MassString[count_string];
+                string[] MassSubString = MassString[1].Split('\t');
+
+                if (count_string <= MassSubString.Length - 1)
+                {
+                    // System.Threading.Thread.Sleep(1000);
+                    textBox2.Text = MassSubString[count_string];
+                    count_string++;
+                }
+                else
+                {
+                    count_string = 0;
+                }
             }
             else
             {
                 MessageBox.Show("Считайте файл");
             }
-            count_string++;
+
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CadCoord aS = new CadCoord();
+            if (filename_f != null)
+            {
+
+                //   textBox2.Text = File.ReadAllText(filename_f);  // TODO выводим содержимое файла в textbox2
+                string[] MassString = File.ReadAllLines(filename_f); // TODO создаем массив строк
+                string[] MassSubString = MassString[1].Split('\t');
+
+                if (count_string <= MassSubString.Length - 1)// заполняем структуру для храниния извлеченных данных из автокада 8 полей
+                {
+                    // System.Threading.Thread.Sleep(1000);
+                    aS.number_p = MassSubString[0];
+                    aS.name_block = MassSubString[1];
+                    aS.cordX = MassSubString[2];
+                    aS.cordY = MassSubString[3];
+                    aS.cordZ = MassSubString[4];
+                    aS.name_kable = MassSubString[5];
+                    aS.beginning = MassSubString[6];
+                    aS.ending = MassSubString[7];
+                    count_string++;
+                    textBox3.Text = aS.name_kable +" "+  aS.beginning + " " + aS.ending;
+                }
+                
+
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Закрываем текущую форму
         }
     }
 }
